cef72cad46db998e26d3812880edb18075435ccd
LeonardoRyuta <leonardo.ryuta05@gmail.com>
2023-07-21 18:36:01 +0200

    router and theme setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,11 @@
-import { ColorModeScript } from "@chakra-ui/react"
+import { ColorModeScript, ChakraProvider } from "@chakra-ui/react"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
-
+import { RouterProvider } from "react-router-dom"
+import router from "./Router"
 
 const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element');
@@ -12,18 +13,12 @@ const root = ReactDOM.createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <ColorModeScript />
-    <App />
+    <ChakraProvider>
+      <RouterProvider router={router}/>
+    </ChakraProvider>
   </React.StrictMode>,
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorker.unregister()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
 
